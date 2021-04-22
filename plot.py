9c5dead81bb70e3853b7917c346e00e09f0a5de6
Eion Blanchard <eionmb2@illinois.edu>
2021-04-22 14:41:55 -0400

    tweaked zone plotting and specific annotations according to mesh degree

--- plot.py
+++ plot.py
@@ -5,32 +5,42 @@ def approximate_mesh(deg1, deg2=None, vals=[], zones=[], filedir=None, filename=
     fig,ax = plt.subplots(figsize=(12,4))
     if not deg2:
         deg2 = deg1
-    for x in range(deg2):
+    for x in range(deg2+1):
         s = np.sin(x)
         if x <= deg1:
             # Plot coarser approximates
-            colorstyle = 'tab:orange'
+            color = 'tab:orange'
             h = 1.
             ax.annotate(str(x), (s,1.1), horizontalalignment="center", verticalalignment="center")
         else:
             # Plot finer approximates
-            colorstyle = 'b'
+            color = 'b'
             h = .8
-        ax.plot((s,s), (-h,h), colorstyle)
+        ax.plot((s,s), (-h,h), color)
+    # Shade zones
+    for a,b in zones:
+        if max(a,b) <= deg1:
+            color = 'darkorange'
+            hp = 0.0775
+        else:
+            color = 'mediumblue'
+            hp = 0.165
+        ax.axvspan(np.sin(a), np.sin(b)-0.007, ymin=hp, ymax=1.-hp, alpha=0.2, color=color)
     # Plot particular values
     for val in vals:
         s = np.sin(val)
-        ax.plot((s,s), (-0.6,0.6), 'darkgoldenrod')
-        ax.annotate(str(val), (s,0.7), horizontalalignment="center", verticalalignment="center")
+        if val > deg2:
+            ax.plot((s,s), (-0.6,0.6), 'gold')
+            h = 0.7
+        else:
+            h = 0.9
+        ax.annotate(str(val), (s,h), horizontalalignment="center", verticalalignment="center")
     # Manage plot
     ax.set_xlim(left=-1.,right=1.)
     ax.set_ylim(bottom=-1.2,top=1.2)
     ax.set_yticks([])
     ax.spines['bottom'].set_position('center')
     ax.spines['top'].set_position('center')
-    # Shade zones
-    for a,b in zones:
-        ax.axvspan(np.sin(a), np.sin(b)-0.007, ymin=0.08, ymax=0.92, alpha=0.2, color='darkorange')
     # Save figure
     if not filedir:
         filedir = './plots/'
