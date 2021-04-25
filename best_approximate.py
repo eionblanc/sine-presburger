fa4ccc41a545b297385ef6424fdc7b778e1ba4de
Eion Blanchard <eionmb2@illinois.edu>
2021-04-25 15:56:09 -0400

    added difference approximations and simplified plots

--- best_approximate.py
+++ best_approximate.py
@@ -22,3 +22,31 @@ def approx(N, d):
     return A
     # Sleeker but slower
     #return np.argmax([np.sin(n) if np.sin(n) <= sine_true else -1 for n in range(d+1)])
+
+def diff_approx(x1, x2, d, e):
+    """
+    Compute the best difference approximates of x1 and x2 under the sine function.
+    That is, minimize |sin(x2) - sin(x1) - |sin(y1) - sin(y2)||,
+    where y1 is at most d and y2 is at most e.
+    :param x1: int with lower sine value for difference approximation
+    :param x2: int with greater sine value for difference approximation
+    :param d: int upper bound for returned y1
+    :param e: int upper bound for returned y2
+    :return y1: int bounded by d comprising a best difference approximate
+    :return y2: int bounded by e comprising a best difference approximate
+    """
+    diff_true = np.sin(x2) - np.sin(x1)
+    y1,y2 = (None,None)
+    if min(x1,x2,d,e) < 0 or diff_true <= 0:
+        return y1,y2
+    err_approx = 2.
+    for n1 in range(d+1):
+        s1 = np.sin(n1)
+        for n2 in range(e+1):
+            s2 = np.sin(n2)
+            error = np.abs(diff_true - np.abs(s1-s2))
+            if error < err_approx:
+                err_approx = error
+                y1 = n1
+                y2 = n2
+    return y1,y2
--- plot.py
+++ plot.py
@@ -1,17 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from best_approximate import *
 
-def approximate_mesh(deg1, deg2=None, vals=[], zones=[], filedir=None, filename=None):
-    fig,ax = plt.subplots(figsize=(12,4))
+def approximate_mesh(deg1, deg2=None, deg1_color='tab:orange', deg1_ann=True,
+                     vals=[], zones=[], filedir=None, filename=None):
+    fig,ax = plt.subplots(figsize=(15,4))
     if not deg2:
         deg2 = deg1
     for x in range(deg2+1):
         s = np.sin(x)
         if x <= deg1:
             # Plot coarser approximates
-            color = 'tab:orange'
+            color = deg1_color
             h = 1.
-            ax.annotate(str(x), (s,1.1), horizontalalignment="center", verticalalignment="center")
+            if deg1_ann:
+                ax.annotate(str(x), (s,1.1), horizontalalignment="center", verticalalignment="center")
         else:
             # Plot finer approximates
             color = 'b'
@@ -20,20 +23,26 @@ def approximate_mesh(deg1, deg2=None, vals=[], zones=[], filedir=None, filename=
     # Shade zones
     for a,b in zones:
         if max(a,b) <= deg1:
-            color = 'darkorange'
+            color = deg1_color
             hp = 0.0775
         else:
             color = 'mediumblue'
             hp = 0.165
-        ax.axvspan(np.sin(a), np.sin(b)-0.007, ymin=hp, ymax=1.-hp, alpha=0.2, color=color)
+        ax.axvspan(np.sin(a), np.sin(b)-0.006, ymin=hp, ymax=1.-hp, alpha=0.1, color=color)
     # Plot particular values
     for val in vals:
         s = np.sin(val)
-        if val > deg2:
-            ax.plot((s,s), (-0.6,0.6), 'gold')
-            h = 0.7
-        else:
+        if val <= deg1:
+            h = 1.1
+        elif val <= deg2:
             h = 0.9
+        else:
+            h = 0.7
+            if deg1 == deg2:
+                ax.plot((s,s), (-0.8,0.8), 'olivedrab')
+                h += 0.2
+            else:
+                ax.plot((s,s), (-0.6,0.6), 'olivedrab')
         ax.annotate(str(val), (s,h), horizontalalignment="center", verticalalignment="center")
     # Manage plot
     ax.set_xlim(left=-1.,right=1.)
@@ -41,10 +50,80 @@ def approximate_mesh(deg1, deg2=None, vals=[], zones=[], filedir=None, filename=
     ax.set_yticks([])
     ax.spines['bottom'].set_position('center')
     ax.spines['top'].set_position('center')
+    ax.set_axisbelow(False)
+    ax.tick_params(length=10, direction='inout')
+    for label in ax.get_xticklabels() + ax.get_yticklabels():
+        label.set_bbox(dict(facecolor='white', edgecolor='None', alpha=0.75))
     # Save figure
     if not filedir:
         filedir = './plots/'
     if not filename:
-        filename = 'mesh-{}'.format(deg1) + ('-{}'.format(deg2) if deg2 != deg1 else '') + '.png'
+        filename = 'mesh-{}'.format(deg1) + ('-{}'.format(deg2) if deg2 != deg1 else '') + '.pdf'
+    fig.savefig(filedir+filename, bbox_inches='tight')
+    return
+
+def diff_approx_mesh(x1, x2, d, e, y1=None, y2=None,
+                     filedir=None, filename=None):
+    s1 = np.sin(x1)
+    s2 = np.sin(x2)
+    if s1 > s2:
+        tmp = x2
+        x2 = x1
+        x1 = tmp
+        tmp = s2
+        s2 = s1
+        s1 = tmp
+    if not (y1 and y2):
+        y1,y2 = diff_approx(x1,x2,d,e)
+    # Plot mesh up to first bound
+    fig,ax = plt.subplots(figsize=(15,4))
+    for x in range(min(d,e)+1):
+        s = np.sin(x)
+        color = 'orange'
+        h = 1.
+        ax.plot((s,s), (-h,h), color)
+    # Plot mesh up to second bound
+    for x in range(min(d,e)+1,max(d,e)+1):
+        s = np.sin(x)
+        color = 'peachpuff'
+        h = 1.
+        ax.plot((s,s), (-h,h), color)
+    # Plot y1, y2 and difference as zone
+    sy1 = np.sin(y1)
+    sy2 = np.sin(y2)
+    ax.plot((sy1,sy1), (-1.,1.), 'mediumblue')
+    ax.annotate(str(y1), (sy1,1.1), horizontalalignment="right", verticalalignment="center")
+    ax.plot((sy2,sy2), (-1.,1.), 'mediumblue')
+    ax.annotate(str(y2), (sy2,1.1), horizontalalignment="left", verticalalignment="center")
+    a = min(sy1,sy2)
+    b = max(sy1,sy2)
+    hp = 0.08
+    ax.axvspan(a, b, ymin=hp, ymax=1.-hp, alpha=0.1, color='b')
+    # Plot x1, x2, and difference as zone
+    ax.plot((s1,s1), (-1.,1.), 'green')
+    if min(np.abs(s1-sy1),np.abs(s1-sy2),np.abs(s2-sy1),np.abs(s2-sy2)) < 0.05:
+        ah = -1.1
+    else:
+        ah = 1.1
+    ax.annotate(str(x1), (s1,ah), horizontalalignment="right", verticalalignment="center")
+    ax.plot((s2,s2), (-1.,1.), 'green')
+    ax.annotate(str(x2), (s2,ah), horizontalalignment="left", verticalalignment="center")
+    hp = 0.08
+    ax.axvspan(s1, s2, ymin=hp, ymax=1.-hp, alpha=0.1, color='seagreen')
+    # Manage plot
+    ax.set_xlim(left=-1.,right=1.)
+    ax.set_ylim(bottom=-1.2,top=1.2)
+    ax.set_yticks([])
+    ax.spines['bottom'].set_position('center')
+    ax.spines['top'].set_position('center')
+    ax.set_axisbelow(False)
+    ax.tick_params(length=10, direction='inout')
+    for label in ax.get_xticklabels() + ax.get_yticklabels():
+        label.set_bbox(dict(facecolor='white', edgecolor='None', alpha=0.75))
+    # Save figure
+    if not filedir:
+        filedir = './plots/'
+    if not filename:
+        filename = 'diffmesh-{}-{}.pdf'.format(x1,x2)
     fig.savefig(filedir+filename, bbox_inches='tight')
     return
